298903975b9ec4ab70d785a6cc1f26292ecd6dcf
FRUCHTBA3R <fefafi@netcologne.de>
2022-06-04 00:24:00 +0200

    Squashed commit of the following:
    commit 4e69f0b400e6d8fd3f87ea66303a877e0977d1f7
    Author: FRUCHTBA3R <fefafi@netcologne.de>
    Date:   Sat Jun 4 00:23:21 2022 +0200
    
        Better font

--- legend_generator.py
+++ legend_generator.py
@@ -1,7 +1,13 @@
 import json
+import math
+
 import numpy as np
 import cv2
+from PIL import ImageFont, ImageDraw, Image
 
+# load font
+bahnschrift = ImageFont.truetype("bahnschrift.ttf", 30)
+bahnschrift.set_variation_by_axes([300, 85])
 
 def generate():
     """Generate legend image from materials configuration."""
@@ -10,19 +16,15 @@ def generate():
         materials = json.load(f)
 
     # font setup
-    fontFace = cv2.FONT_HERSHEY_SIMPLEX
-    fontScale = 0.8 #0.4375  # as seen in bp_to_img > __create_images
-
     max_width = 0
-    max_height = 0
-    max_baseline = 0
     for k in materials:
         # get size
-        (width, height), baseline = cv2.getTextSize(k, fontFace, fontScale, 2)
-        height += baseline
-        max_width = max(max_width, width)
-        max_height = max(max_height, height)
-        max_baseline = max(max_baseline, baseline)
+        max_width = max(max_width, bahnschrift.getlength(k, "L"))
+
+    max_width = math.ceil(max_width)
+    max_height, max_baseline = bahnschrift.getmetrics()
+    max_height += max_baseline
+
 
     # image setup
     front_space = 15
@@ -34,21 +36,23 @@ def generate():
                 front_space + max_height + 5 + max_width + back_space,
                 3)
     img = np.full(img_size, background_color, dtype=np.uint8)
+    pilimg = Image.fromarray(img[:, :, ::-1])
+    draw = ImageDraw.Draw(pilimg)
 
     # image creation
     current_y = top_space
     text_loc_x = front_space + max_height + 5
-    text_loc_y = max_height - max_baseline
     for k in materials:
         # fill square with material color
-        img[current_y:current_y+max_height, front_space:front_space+max_height] = materials[k]["Color"]
+        draw.rectangle((front_space, current_y, front_space+max_height, current_y+max_height),
+                       fill=tuple(materials[k]["Color"][::-1]))
         # put text
-        cv2.putText(img, k, (text_loc_x, current_y + text_loc_y), fontFace, fontScale, (255, 255, 255), 2)
+        draw.text((text_loc_x, current_y), k, font=bahnschrift)
         # next
         current_y += 5 + max_height
 
     # show / save
-    cv2.imwrite("legend.png", img)
+    pilimg.save("legend.png")
 
 
 if __name__ == "__main__":
